70a837e00d5075f05ed8837c7e7c594750c0931e
Aarsh-P <aarshjpatel@gmail.com>
2025-02-06 00:58:39 +0530

    product yup validation

--- Exercises/ecom/validationSchema/productSchema.js
+++ Exercises/ecom/validationSchema/productSchema.js
@@ -1,16 +1,15 @@
 const yup = require("yup");
-const { createCategory } = require("../controllers/categoryController");
 
 
 const productCreateSchema = yup.object({
     body: yup.object({
-        name: yup.string().required("first_name is required"),
+        name: yup.string().required("name is required"),
         description: yup.string().optional(),
         price: yup.number("price must be number").required("price is required").positive(),
         stock: yup.number("stock must be number").required("stock is required").positive(),
         category_id: yup.number("category_id must be int").required("category_id is required").positive(),
         quantity: yup.number("quantity must be int").required("quantity is required").positive(),
-        image_url: yup.string("quantity must be string").optional()
+        image_url: yup.string("image_url must be string").optional()
     }),
 });
 
@@ -22,7 +21,7 @@ const productUpdateSchema = yup.object({
         stock: yup.number("stock must be number").optional().positive(),
         category_id: yup.number("category_id must be int").optional().positive(),
         quantity: yup.number("quantity must be int").optional().positive(),
-        image_url: yup.string("quantity must be string").optional()
+        image_url: yup.string("image_url must be string").optional()
     }),
     params: yup.object({
         id: yup.number().required("id is required").positive("id must be a positive integer")
