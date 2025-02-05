ef9a64f2b7529aeb2a3d7c79899411062ff12698
Aarsh-P <aarshjpatel@gmail.com>
2025-02-06 00:57:00 +0530

    converting form data to raw-json

--- Exercises/ecom/validators/validator.js
+++ Exercises/ecom/validators/validator.js
@@ -1,5 +1,14 @@
 const validator = (schema) => async (req, res, next) => {
     try {
+        if (req.is('multipart/form-data')) { // Only run for form-data
+            req.body = {
+              ...req.body,
+              price: req.body.price ? parseFloat(req.body.price) : undefined,
+              stock: req.body.stock ? parseInt(req.body.stock, 10) : undefined,
+              category_id: req.body.category_id ? parseInt(req.body.category_id, 10) : undefined,
+              quantity: req.body.quantity ? parseInt(req.body.quantity, 10) : undefined,
+            };
+          }
         await schema.validate({
             body: req.body,
             params: req.params,
